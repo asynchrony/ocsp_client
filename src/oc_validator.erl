305c69cb6eb49edaaac7f4a7bb9b60bdefe3341d
Nate McKie <nate.mckie@asolutions.com>
2011-12-19 12:44:59 -0600

    Returns ok if any error comes on HTTP request.  -Nate

--- src/oc_validator.erl
+++ src/oc_validator.erl
@@ -8,10 +8,14 @@ validate_cert(PeerCert) ->
     {IssuerName, IssuerKey, SerialNumber} = oc_request_data:get_request_data(PeerCert),
     AssembledRequest = oc_request_assembler:assemble_request(IssuerName, IssuerKey, SerialNumber),
     RequestBytes = 'OCSP':encode('OCSPRequest', AssembledRequest),
-    Response = httpc:request(post, {?OCSP_URL, "application/ocsp-request", [], RequestBytes}, [], []),
-    case oc_response_parser:parse(Response) of
-        {revoked, {_Reason, _, _}} ->
-            {error, revoked};
+    case httpc:request(post, {?OCSP_URL, "application/ocsp-request", [], RequestBytes}, [], []) of
+        {ok, Response} ->
+            case oc_response_parser:parse(Response) of
+                {revoked, {_Reason, _, _}} ->
+                    {error, revoked};
+                _ ->
+                    ok
+            end;
         _ ->
             ok
     end.
--- test/oc_validator_test.erl
+++ test/oc_validator_test.erl
@@ -9,7 +9,7 @@ validate_cert_returns_error_when_revoke_message_returned_from_provider() ->
     stub(oc_request_data, get_request_data, 1, {issuername, issuerkey, serialnumber}),
     stub(oc_request_assembler, assemble_request, 3, assembled_request),
     stub('OCSP', encode, 2, request_bytes),
-    stub(httpc, request, 4, ocsp_response),
+    stub(httpc, request, 4, {ok, ocsp_response}),
     stub(oc_response_parser, parse, 1, {revoked, {'Reason', timestamp, asn1_NOVALUE}}),
 
     ?assertMatch({error, revoked}, oc_validator:validate_cert(peercert)),
@@ -24,7 +24,7 @@ validate_cert_returns_ok_when_good_returned_from_provider() ->
     stub(oc_request_data, get_request_data, 1, {issuername, issuerkey, serialnumber}),
     stub(oc_request_assembler, assemble_request, 3, assembled_request),
     stub('OCSP', encode, 2, request_bytes),
-    stub(httpc, request, 4, ocsp_response),
+    stub(httpc, request, 4, {ok, ocsp_response}),
     stub(oc_response_parser, parse, 1, {good, 'NULL'}),
 
     ?assertMatch(ok, oc_validator:validate_cert(peercert)),
@@ -34,4 +34,17 @@ validate_cert_returns_ok_when_good_returned_from_provider() ->
     ?assert(meck:called('OCSP', encode, ['OCSPRequest', assembled_request])),
     ?assert(meck:called(httpc, request, [post, {"http://localhost:8088", "application/ocsp-request", [], request_bytes}, [], []])),
     ?assert(meck:called(oc_response_parser, parse, [ocsp_response])).
+
+validate_cert_returns_ok_when_provider_request_returns_error() ->
+    stub(oc_request_data, get_request_data, 1, {issuername, issuerkey, serialnumber}),
+    stub(oc_request_assembler, assemble_request, 3, assembled_request),
+    stub('OCSP', encode, 2, request_bytes),
+    stub(httpc, request, 4, {error, "reason"}),
+
+    ?assertMatch(ok, oc_validator:validate_cert(peercert)),
+
+    ?assert(meck:called(oc_request_data, get_request_data, [peercert])),
+    ?assert(meck:called(oc_request_assembler, assemble_request, [issuername, issuerkey, serialnumber])),
+    ?assert(meck:called('OCSP', encode, ['OCSPRequest', assembled_request])),
+    ?assert(meck:called(httpc, request, [post, {"http://localhost:8088", "application/ocsp-request", [], request_bytes}, [], []])).
 
