88fe3555aab8323720b8cef05d9e54af1822c788
Matt Campbell <matthew.campbell@asynchrony.com>
2013-02-28 16:23:18 -0600

    Move OCSP URL lookup outside oc_validator

--- src/oc_validator.erl
+++ src/oc_validator.erl
@@ -1,12 +1,11 @@
 -module(oc_validator).
--export([validate_cert/1]).
+-export([validate_cert/2]).
 -include("OCSP.hrl").
 
-validate_cert(PeerCert) ->
+validate_cert(PeerCert, ProviderURL) ->
     {IssuerName, IssuerKey, SerialNumber} = oc_request_data:get_request_data(PeerCert),
     AssembledRequest = oc_request_assembler:assemble_request(IssuerName, IssuerKey, SerialNumber),
     {ok, RequestBytes} = 'OCSP':encode('OCSPRequest', AssembledRequest),
-    {ok, ProviderURL} = application:get_env(ocsp_client, ocsp_provider_url),
     case httpc:request(post, {ProviderURL, [], "application/ocsp-request", RequestBytes}, [], []) of
         {ok, {{_Version, 200, _Description}, _Headers, Response}} ->
             case oc_response_parser:parse(Response) of
@@ -15,7 +14,9 @@ validate_cert(PeerCert) ->
                 _ ->
                     ok
             end;
-        _Error ->
-            ok
+        {ok, {{_Version, HttpCode, Description}, _Headers, Response}} ->
+            {error, {HttpCode, Description, Response}};
+        {error, Reason} ->
+            {error, Reason}
     end.
 
--- test/oc_validator_test.erl
+++ test/oc_validator_test.erl
@@ -9,16 +9,14 @@ validate_cert_returns_error_when_revoke_message_returned_from_provider() ->
     stub(oc_request_data, get_request_data, 1, {issuername, issuerkey, serialnumber}),
     stub(oc_request_assembler, assemble_request, 3, assembled_request),
     stub('OCSP', encode, 2, {ok, request_bytes}),
-    stub(application, get_env, 2, {ok, "provider url"}),
     stub(httpc, request, 4, {ok, {{version, 200, description}, headers, ocsp_response}}),
     stub(oc_response_parser, parse, 1, {revoked, {'Reason', timestamp, asn1_NOVALUE}}),
 
-    ?assertMatch({error, revoked}, oc_validator:validate_cert(peercert)),
+    ?assertMatch({error, revoked}, oc_validator:validate_cert(peercert, "provider url")),
 
     ?assert(meck:called(oc_request_data, get_request_data, [peercert])),
     ?assert(meck:called(oc_request_assembler, assemble_request, [issuername, issuerkey, serialnumber])),
     ?assert(meck:called('OCSP', encode, ['OCSPRequest', assembled_request])),
-    ?assert(meck:called(application, get_env, [ocsp_client, ocsp_provider_url])),
     ?assert(meck:called(httpc, request, [post, {"provider url", [], "application/ocsp-request", request_bytes}, [], []])),
     ?assert(meck:called(oc_response_parser, parse, [ocsp_response])).
 
@@ -26,29 +24,37 @@ validate_cert_returns_ok_when_good_returned_from_provider() ->
     stub(oc_request_data, get_request_data, 1, {issuername, issuerkey, serialnumber}),
     stub(oc_request_assembler, assemble_request, 3, assembled_request),
     stub('OCSP', encode, 2, {ok, request_bytes}),
-    stub(application, get_env, 2, {ok, "provider url"}),
     stub(httpc, request, 4, {ok, {{version, 200, description}, headers, ocsp_response}}),
     stub(oc_response_parser, parse, 1, {good, 'NULL'}),
 
-    ?assertMatch(ok, oc_validator:validate_cert(peercert)),
+    ?assertMatch(ok, oc_validator:validate_cert(peercert, "provider url")),
 
     ?assert(meck:called(oc_request_data, get_request_data, [peercert])),
     ?assert(meck:called(oc_request_assembler, assemble_request, [issuername, issuerkey, serialnumber])),
     ?assert(meck:called('OCSP', encode, ['OCSPRequest', assembled_request])),
-    ?assert(meck:called(application, get_env, [ocsp_client, ocsp_provider_url])),
     ?assert(meck:called(oc_response_parser, parse, [ocsp_response])).
 
-validate_cert_returns_ok_when_provider_request_returns_error() ->
+validate_cert_returns_error_when_provider_request_returns_error() ->
     stub(oc_request_data, get_request_data, 1, {issuername, issuerkey, serialnumber}),
     stub(oc_request_assembler, assemble_request, 3, assembled_request),
     stub('OCSP', encode, 2, {ok, request_bytes}),
-    stub(application, get_env, 2, {ok, "provider url"}),
     stub(httpc, request, 4, {error, "reason"}),
 
-    ?assertMatch(ok, oc_validator:validate_cert(peercert)),
+    ?assertMatch({error, "reason"}, oc_validator:validate_cert(peercert, "provider url")),
 
     ?assert(meck:called(oc_request_data, get_request_data, [peercert])),
     ?assert(meck:called(oc_request_assembler, assemble_request, [issuername, issuerkey, serialnumber])),
-    ?assert(meck:called('OCSP', encode, ['OCSPRequest', assembled_request])),
-    ?assert(meck:called(application, get_env, [ocsp_client, ocsp_provider_url])).
+    ?assert(meck:called('OCSP', encode, ['OCSPRequest', assembled_request])).
+
+validate_cert_returns_error_when_provider_request_returns_non_200_http_code() ->
+    stub(oc_request_data, get_request_data, 1, {issuername, issuerkey, serialnumber}),
+    stub(oc_request_assembler, assemble_request, 3, assembled_request),
+    stub('OCSP', encode, 2, {ok, request_bytes}),
+    stub(httpc, request, 4, {ok, {{version, 500, description}, headers, error_response}}),
+
+    ?assertMatch({error, {500, description, error_response}}, oc_validator:validate_cert(peercert, "provider url")),
+
+    ?assert(meck:called(oc_request_data, get_request_data, [peercert])),
+    ?assert(meck:called(oc_request_assembler, assemble_request, [issuername, issuerkey, serialnumber])),
+    ?assert(meck:called('OCSP', encode, ['OCSPRequest', assembled_request])).
 
