b10250c470ac6ea996c863f47edd6f6e9daf6191
Matt Campbell <matthew.campbell@asynchrony.com>
2013-03-05 18:00:45 -0600

    Convert oc_validator to use oc_nonce:generate

--- src/oc_validator.erl
+++ src/oc_validator.erl
@@ -3,7 +3,7 @@
 -include("OCSP.hrl").
 
 validate_cert(PeerCert, CAChain, RequestorCert, RequestorPrivateKey, ProviderURL) ->
-    Nonce = oc_request_data:generate_crypto_nonce(),
+    Nonce = oc_nonce:generate(),
     RequestBytes = oc_request_assembler:assemble_request(PeerCert, CAChain, RequestorCert, RequestorPrivateKey, Nonce),
     case httpc:request(post, {ProviderURL, [], "application/ocsp-request", RequestBytes}, [], []) of
         {ok, {{_Version, HttpCode, Description}, _Headers, Response}} ->
--- test/oc_validator_test.erl
+++ test/oc_validator_test.erl
@@ -6,7 +6,7 @@
 ?MOCK_FIXTURE.
 
 validate_cert_returns_error_when_provider_request_returns_error() ->
-    stub(oc_request_data, generate_crypto_nonce, 0, crypto_nonce),
+    stub(oc_nonce, generate, 0, crypto_nonce),
     stub(oc_request_assembler, assemble_request, 5, assembled_request),
     stub(httpc, request, 4, {error, "reason"}),
 
@@ -16,7 +16,7 @@ validate_cert_returns_error_when_provider_request_returns_error() ->
     ?assert(meck:called(oc_request_assembler, assemble_request, [peercert, ca_chain, requestor_cert, requestor_key, crypto_nonce])).
 
 validate_cert_returns_error_when_provider_request_returns_non_200_http_code() ->
-    stub(oc_request_data, generate_crypto_nonce, 0, crypto_nonce),
+    stub(oc_nonce, generate, 0, crypto_nonce),
     stub(oc_request_assembler, assemble_request, 5, assembled_request),
     stub(httpc, request, 4, {ok, {{version, 500, description}, headers, error_response}}),
 
@@ -26,7 +26,7 @@ validate_cert_returns_error_when_provider_request_returns_non_200_http_code() ->
     ?assert(meck:called(oc_request_assembler, assemble_request, [peercert, ca_chain, requestor_cert, requestor_key, crypto_nonce])).
 
 validate_cert_returns_parser_result_when_request_returns_200_http_code() ->
-    stub(oc_request_data, generate_crypto_nonce, 0, crypto_nonce),
+    stub(oc_nonce, generate, 0, crypto_nonce),
     stub(oc_request_assembler, assemble_request, 5, assembled_request),
     stub(httpc, request, 4, {ok, {{version, 200, description}, headers, ocsp_response}}),
     stub(oc_response_parser, parse, 2, parser_result),
