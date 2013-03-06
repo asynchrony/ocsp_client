6fe4bd0d9485d0c08633c41cfa84f25fdc3e7aa9
Matt Campbell <matthew.campbell@asynchrony.com>
2013-03-06 13:01:26 -0600

    Change tests to use test_helper funcs

--- test/oc_response_parser_test.erl
+++ test/oc_response_parser_test.erl
@@ -8,7 +8,7 @@
 -define(UNKNOWN_RESPONSE_NONCE, <<4,16,131,133,19,224,146,246,192,125,115,215,159,63,28,170,94,155>>).
 
 parse_should_return_ok_when_cert_status_is_good_test() ->
-    {ok, Response} = file:read_file("../test/data/good_response.der"),
+    Response = test_support:read_data("good_response.der"),
     ?assertMatch(ok, oc_response_parser:parse(Response, ?GOOD_RESPONSE_NONCE)).
 
 parse_should_return_error_when_response_status_not_successful_test() ->
@@ -29,13 +29,13 @@ parse_should_return_error_when_unhandled_response_type_test() ->
     ?assertMatch({error, {ocsp, unhandled_response_type}}, oc_response_parser:parse(Response, nonce)).
 
 parse_should_return_error_when_cert_status_is_revoked_test() ->
-    {ok, Response} = file:read_file("../test/data/revoked_response.der"),
+    Response = test_support:read_data("revoked_response.der"),
     ?assertMatch({error, certificate_revoked}, oc_response_parser:parse(Response, ?REVOKED_RESPONSE_NONCE)).
 
 parse_should_return_error_when_cert_status_is_unknown_test() ->
-    {ok, Response} = file:read_file("../test/data/unknown_response.der"),
+    Response = test_support:read_data("unknown_response.der"),
     ?assertMatch({error, certificate_unknown_by_ocsp}, oc_response_parser:parse(Response, ?UNKNOWN_RESPONSE_NONCE)).
 
 parse_should_return_error_when_nonce_does_not_match_test() ->
-    {ok, Response} = file:read_file("../test/data/good_response.der"),
+    Response = test_support:read_data("good_response.der"),
     ?assertMatch({error, {ocsp, nonce_mismatch}}, oc_response_parser:parse(Response, ?UNKNOWN_RESPONSE_NONCE)).
