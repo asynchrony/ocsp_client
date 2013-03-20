0ad52b99688e095f543c433f7cd192941d34b3c8
Matt Campbell <matthew.campbell@asynchrony.com>
2013-03-20 17:32:00 -0500

    Convert oc_validator_test from meck to hoax

--- test/oc_validator_test.erl
+++ test/oc_validator_test.erl
@@ -1,45 +1,59 @@
 -module(oc_validator_test).
 -compile([export_all]).
--include_lib("test_support/include/test_helper.hrl").
+-include_lib("eunit/include/eunit.hrl").
+-include_lib("hoax/include/hoax.hrl").
 -include("OCSP.hrl").
 
-?MOCK_FIXTURE.
+?HOAX_FIXTURE(setup, teardown).
+
+setup() ->
+    mock(oc_request_assembler, [
+            ?expect(assemble_cert_id,
+                ?withArgs([peercert, ca_chain]),
+                ?andReturn(cert_id)),
+            ?expect(assemble_request,
+                ?withArgs([cert_id, requestor_cert, requestor_key, crypto_nonce]),
+                ?andReturn(assembled_request))
+        ]),
+    mock(oc_nonce, [
+            ?expect(generate, ?withArgs([]), ?andReturn(crypto_nonce))
+        ]),
+    ok.
+
+teardown(_) ->
+    ok.
+
+-define(expectHttpcToReturn(Result),
+    mock(httpc, [
+            ?expect(request,
+                ?withArgs([post, {"provider url", [], "application/ocsp-request", assembled_request}, [], []]),
+                ?andReturn(Result))
+        ])
+).
 
 validate_cert_returns_error_when_provider_request_returns_error() ->
-    stub(oc_request_assembler, assemble_cert_id, 2, cert_id),
-    stub(oc_nonce, generate, 0, crypto_nonce),
-    stub(oc_request_assembler, assemble_request, 4, assembled_request),
-    stub(httpc, request, 4, {error, "reason"}),
+    ?expectHttpcToReturn({error, "reason"}),
 
-    ?assertMatch({error, {ocsp, "reason"}}, oc_validator:validate_cert(peercert, ca_chain, requestor_cert, requestor_key, "provider url")),
+    Result = oc_validator:validate_cert(peercert, ca_chain, requestor_cert, requestor_key, "provider url"),
 
-    ?assert(meck:called(httpc, request, [post, {"provider url", [], "application/ocsp-request", assembled_request}, [], []])),
-    ?assert(meck:called(oc_request_assembler, assemble_cert_id, [peercert, ca_chain])),
-    ?assert(meck:called(oc_request_assembler, assemble_request, [cert_id, requestor_cert, requestor_key, crypto_nonce])).
+    ?assertMatch({error, {ocsp, "reason"}}, Result),
+    ?verifyAll.
 
 validate_cert_returns_error_when_provider_request_returns_non_200_http_code() ->
-    stub(oc_request_assembler, assemble_cert_id, 2, cert_id),
-    stub(oc_nonce, generate, 0, crypto_nonce),
-    stub(oc_request_assembler, assemble_request, 4, assembled_request),
-    stub(httpc, request, 4, {ok, {{version, 500, description}, headers, error_response}}),
+    ?expectHttpcToReturn({ok, {{version, 500, description}, headers, error_response}}),
 
-    ?assertMatch({error, {ocsp, {500, description, error_response}}}, oc_validator:validate_cert(peercert, ca_chain, requestor_cert, requestor_key, "provider url")),
+    Result = oc_validator:validate_cert(peercert, ca_chain, requestor_cert, requestor_key, "provider url"),
 
-    ?assert(meck:called(httpc, request, [post, {"provider url", [], "application/ocsp-request", assembled_request}, [], []])),
-    ?assert(meck:called(oc_request_assembler, assemble_cert_id, [peercert, ca_chain])),
-    ?assert(meck:called(oc_request_assembler, assemble_request, [cert_id, requestor_cert, requestor_key, crypto_nonce])).
+    ?assertMatch({error, {ocsp, {500, description, error_response}}}, Result),
+    ?verifyAll.
 
-validate_cert_returns_validater_result_when_request_returns_200_http_code() ->
-    stub(oc_request_assembler, assemble_cert_id, 2, cert_id),
-    stub(oc_nonce, generate, 0, crypto_nonce),
-    stub(oc_request_assembler, assemble_request, 4, assembled_request),
-    stub(httpc, request, 4, {ok, {{version, 200, description}, headers, ocsp_response}}),
-    stub(oc_response_validator, validate, 3, validator_result),
+validate_cert_returns_validator_result_when_request_returns_200_http_code() ->
+    ?expectHttpcToReturn({ok, {{version, 200, description}, headers, ocsp_response}}),
+    mock(oc_response_validator, [
+            ?expect(validate, ?withArgs([ocsp_response, cert_id, crypto_nonce]), ?andReturn(validator_result))
+        ]),
 
-    ?assertMatch(validator_result, oc_validator:validate_cert(peercert, ca_chain, requestor_cert, requestor_key, "provider url")),
+    Result = oc_validator:validate_cert(peercert, ca_chain, requestor_cert, requestor_key, "provider url"),
 
-    ?assert(meck:called(httpc, request, [post, {"provider url", [], "application/ocsp-request", assembled_request}, [], []])),
-    ?assert(meck:called(oc_request_assembler, assemble_request, [cert_id, requestor_cert, requestor_key, crypto_nonce])),
-    ?assert(meck:called(oc_request_assembler, assemble_cert_id, [peercert, ca_chain])),
-    ?assert(meck:called(oc_response_validator, validate, [ocsp_response, cert_id, crypto_nonce])).
-
+    ?assertMatch(validator_result, Result),
+    ?verifyAll.
